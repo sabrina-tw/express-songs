24ded2e0ed73870100143593678198d5453376d8
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-01-04 17:24:26 +0800

    add middleware

--- index.js
+++ index.js
@@ -15,6 +15,38 @@ const songs = [
 
 // const songs = [];
 
+// MIDDLEWARE
+app.use((req, res, next) => {
+  console.log("common middleware function was called!");
+  next();
+});
+
+const checkColor = () => {
+  console.log("in checkColor");
+  return (req, res, next) => {
+    if (req.query.color) {
+      res.send("I have color");
+    } else {
+      next();
+    }
+  };
+};
+
+app.use(checkColor());
+
+const requireJsonContent = (req, res, next) => {
+  if (req.headers["content-type"] !== "application/json") {
+    res.status(400).send("Server wants application/json!");
+  } else {
+    next();
+  }
+};
+
+// ROUTES
+app.post("/songs", requireJsonContent, (req, res, next) => {
+  res.send("thanks for the json!");
+});
+
 app.get("/songs", (req, res) => {
   res.status(200).json(songs);
 });
