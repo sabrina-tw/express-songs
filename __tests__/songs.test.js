f1c24421db3518d10b871eb39861074b4fe41212
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-01-05 10:13:27 +0800

    Implement tests for songs routes

---
+++ __tests__/songs.test.js
@@ -0,0 +1,58 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("songs", () => {
+  describe("POST /songs", () => {
+    it("should add a new song and return the new song object", async () => {
+      const newSong = { name: "Pink Moon", artist: "Nick Drake" };
+      const expectedSong = { id: 3, name: "Pink Moon", artist: "Nick Drake" };
+
+      const response = await request(app)
+        .post("/songs")
+        .send(newSong)
+        .expect(201);
+
+      expect(response.status).toEqual(201);
+      expect(response.body).toEqual(expectedSong);
+    });
+  });
+
+  describe("GET /song/:id", () => {
+    it("should get correct song given id", async () => {
+      const expectedSong = { name: "Pink Moon", artist: "Nick Drake" };
+
+      const { body: actualSong } = await request(app)
+        .get("/songs/3")
+        .expect(200);
+
+      expect(actualSong).toMatchObject(expectedSong);
+    });
+  });
+
+  const agent = request.agent(app);
+
+  describe("PUT /song/:id", () => {
+    it("should modify correct song successfully given id", async () => {
+      const modifiedSong = { name: "Pink Moon edited", artist: "Nick Drake" };
+
+      const response = await agent
+        .put("/songs/3")
+        .send(modifiedSong)
+        .expect(200);
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toMatchObject(modifiedSong);
+    });
+  });
+
+  describe("DELETE /song/:id", () => {
+    it("should delete correct song successfully given id", async () => {
+      const songToDelete = { name: "Pink Moon edited", artist: "Nick Drake" };
+
+      const response = await agent.delete("/songs/3").expect(200);
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toMatchObject(songToDelete);
+    });
+  });
+});
